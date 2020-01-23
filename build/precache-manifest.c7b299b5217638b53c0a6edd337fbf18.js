80dfe205c21d4fbd5db4e915d4197701edeb1baf
Ritesh Meena <45878657+rtm20@users.noreply.github.com>
2020-01-23 21:32:31 +0530

    Add files via upload

---
+++ build/precache-manifest.c7b299b5217638b53c0a6edd337fbf18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42540d842833ede46b63a1cf43248f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3574d4ef346d417e13e6",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b9d24385ee42b6f48eb5",
+    "url": "/static/css/main.2e0b0e4c.chunk.css"
+  },
+  {
+    "revision": "3574d4ef346d417e13e6",
+    "url": "/static/js/2.087fb113.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.087fb113.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9d24385ee42b6f48eb5",
+    "url": "/static/js/main.9c692a9d.chunk.js"
+  },
+  {
+    "revision": "882838377396d1330cb8",
+    "url": "/static/js/runtime-main.ff00ab8c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
